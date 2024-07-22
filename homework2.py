c51677d8d106e8a8ce1a12265e8747c9bb6b572d
RomanAnoshenko <89294907361@mail.ru>
2024-07-22 23:01:32 +0900

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+kurs_name = "Курс: Python"
+time_work = 1.5
+home_work = 12
+print(kurs_name, "Количество затраченных часов",time_work,"Количество выполненных ДЗ", home_work, "Время на одно задание",(time_work*60)/home_work, "мин")
